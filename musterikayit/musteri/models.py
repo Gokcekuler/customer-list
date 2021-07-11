f3554fc04210a3c7a3366adbf08b63566160ea3b
Gokce Kuler <gokcekuler@gmail.com>
2021-07-12 00:15:53 +0300

    ön yüz için iyleştirmeler ve girdi alanları için kontroller eklendi

--- musterikayit/musteri/models.py
+++ musterikayit/musteri/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Musteri(models.Model):
-    tc=models.PositiveSmallIntegerField()
+    tc=models.PositiveSmallIntegerField( validators=[ MinValueValidator(10000000000),MaxValueValidator(99999999999)], unique=True)
     isim=models.CharField(max_length=100)
     soyisim=models.CharField(max_length=100)
-    telefon=models.PositiveSmallIntegerField()
+    telefon=models.PositiveSmallIntegerField(validators=[ MinValueValidator(1000000000), MaxValueValidator(9999999999)])
     sehir=models.CharField(max_length=100)
     ilce=models.CharField(max_length=100)
-
 
     def __str__(self):
         return self.isim
@@ -18,4 +17,12 @@ class Musteri(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs= {'id': self.id})
 
-# Create your models here.
+    def get_create_url(self):
+        return reverse('create')
+
+    def get_update_url(self):
+        return reverse('update', kwargs= {'id': self.id})
+
+    def get_delete_url(self):
+        return reverse('delete', kwargs= {'id': self.id})
+
--- musterikayit/musteri/urls.py
+++ musterikayit/musteri/urls.py
@@ -5,12 +5,12 @@ from .views import *
 
 urlpatterns = [
 
-        
-        url('index/', musteri_index, name= 'index'),
-        url(r'^(?P<id>\d+)/detail/$', musteri_detail, name='detail'),
-        url('create/', musteri_create, name='create'),
-        url(r'^(?P<id>\d+)/update/$',musteri_update,name = 'update'),
-        url(r'^(?P<id>\d+)/delete/$', musteri_delete, name='delete'),
 
-        
+        url('index/', customer_index, name= 'index'),
+        url(r'^(?P<id>\d+)/detail/$', customer_detail, name='detail'),
+        url('create/', customer_create, name='create'),
+        url(r'^(?P<id>\d+)/update/$',customer_update,name = 'update'),
+        url(r'^(?P<id>\d+)/delete/$', customer_delete, name='delete'),
+
+     
 ]
